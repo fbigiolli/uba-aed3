a0585f9ed42467e397fb0c6a63ad54d91d5e13d9
fbigiolli <felipebigiolli@gmail.com>
2023-08-20 13:52:22 -0300

    ej1 practica 1

---
+++ Practicas/Practica 1/ej1.cpp
@@ -0,0 +1,48 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+// La implementacion va a tomar como entrada un vector, ya que no hay un modulo de multiconjunto en la STL.
+
+bool exists_subset_sum(vector<int>& set, int i, int j, vector<int>& subset)
+{
+    if (j < 0) return false; //  Regla de factibilidad para no considerar aquellos conjuntos que se pasan de la suma
+
+    if(j == 0) // Print del subset valido
+    {
+        for(auto num : subset)
+        {
+            cout << num << " ";
+        }      
+        return true;
+    }
+
+    if (i == 0) return false; // No hay mas posiciones para recorrer
+
+    // Recursion incluyendo el elemento actual al subset
+    subset.push_back(set[i-1]);
+    bool included = exists_subset_sum(set,i-1,j - set[i-1],subset);
+    subset.pop_back();
+
+    // Recursion no incluyendo el elemento actual al subset
+    bool not_included = exists_subset_sum(set,i-1,j,subset);
+
+    return included or not_included;
+}
+
+
+// Driver code
+
+int main()
+{
+    vector<int> set;
+    for (int i = 0; i < 15; i++)
+    {
+        set.push_back(i);
+    }
+    
+    int k = 9;
+    vector<int> subset;
+    if(!exists_subset_sum(set,set.size(),k,subset)) cout << "No existe un subconjunto que sume " << k << endl;
+    return 0;
+}
