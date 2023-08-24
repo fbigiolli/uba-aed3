fb599b53a4876938f52d4a54c72aec2e4e9cb17b
fbigiolli <felipebigiolli@gmail.com>
2023-08-24 00:39:07 -0300

    ej3 corregido

--- Practicas/Practica 1/ej3.cpp
+++ Practicas/Practica 1/ej3.cpp
@@ -5,7 +5,8 @@ using namespace std;
 vector<int> res;
 int max_sum = -1;
 
-vector<int> matrix_max_subset_sum(vector<vector<int>>& mat, int k, int i, vector<int>&subset, int sum)
+// Algoritmo top-down backtracking. El algoritmo hace varios recorridos de mas ya que genera todos los subsets posibles entre i y n, mas alla de aquellos que sean mayores al k.
+void matrix_max_subset_sum(vector<vector<int>>& mat, int k, int i, vector<int>&subset, int sum)
 {
     int n = mat.size();
 
@@ -19,23 +20,28 @@ vector<int> matrix_max_subset_sum(vector<vector<int>>& mat, int k, int i, vector
             max_sum = sum;
         }
     }
-    // Ademas, si i es menor al numero de filas/columnas de la matriz
-    else if (i < n)
-    {
-        // Agrego i al subset
-        subset.push_back(i);
-        int new_element_sum = 0;
 
-        // Calculo la suma de las posibles combinaciones de i con los elementos del subset
-        for (int j = 0; j < subset.size(); j++)
+    // Ademas, si i es menor al numero de filas/columnas de la matriz
+    else
+    {
+        // No es un else if porque si no el algoritmo recorreria subsets innecesarios en los que hay mas elementos de los que k permite en ciertos casos.
+        if (i < n)
         {
-            new_element_sum += mat[i][subset[j]] * 2; // la matriz es simetrica, no hace falta acceder a la posicion espejo
+             // Agrego i al subset
+            subset.push_back(i);
+            int new_element_sum = 0;
+
+            // Calculo la suma de las posibles combinaciones de i con los elementos del subset
+            for (int j = 0; j < subset.size(); j++)
+            {
+                new_element_sum += mat[i][subset[j]] + mat[subset[j]][i]; // la matriz es simetrica, no hace falta acceder a la posicion espejo
+            }
+            
+            // Recursion + backtracking
+            matrix_max_subset_sum(mat,k-1,i+1,subset,sum+new_element_sum);
+            subset.pop_back();
+            matrix_max_subset_sum(mat,k,i+1,subset,sum);
         }
-        
-        // Recursion + backtracking
-        matrix_max_subset_sum(mat,k-1,i+1,subset,sum+new_element_sum);
-        subset.pop_back();
-        matrix_max_subset_sum(mat,k,i+1,subset,sum);
     }
 }
 
@@ -53,11 +59,13 @@ int main(int argc, char const *argv[])
     matrix_max_subset_sum(mat,k,0,subset,0);
 
      // Print solution.
-    for (int i = 0; i < subset.size(); i++)
-    {
-        cout << subset[i] ;
+    for (int i = 0; i < res.size(); i++)
+    {   
+        // Aca en realidad habria que sumarle 1 a cada output para cumplir con el enunciado, porque arranca a indexar desde el 0
+        cout << res[i] << " " ;
     }
     
 
     return 0;
 }
+
